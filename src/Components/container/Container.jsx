0688a671c9573a05d2cfa54f5ba2fdc3aa072e3e
vikash kumar <viku81021@gmail.com>
2023-11-07 12:03:34 +0530

    "add footer"

---
+++ src/Components/container/Container.jsx
@@ -0,0 +1,9 @@
+import React, { Children } from 'react'
+
+export default function Container({children}) {
+  return (
+    <div className='w-full max-w-7xl mx-auto px-4'>
+      {children}
+    </div>
+  )
+}
